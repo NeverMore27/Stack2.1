be181ef25b5db80761175c165e6543c8788c371f
NeverMore27 <uragan.uragan.59@mail.ru>
2017-05-29 13:06:44 +0300

    Update vector.hpp

--- include/vector.hpp
+++ include/vector.hpp
@@ -1,19 +1,19 @@
-#include <iostream>
-
-class vector_t 
+class vector_t
 {
 private:
-    unsigned int size_;
-    unsigned int capacity_;
-    int* ptr_;
+	unsigned int size_;
+	unsigned int capacity_;
+	int* ptr_;
 public:
-    vector_t() noexcept;
-    vector_t(unsigned int size);
-    vector_t(const vector_t& other);
-    auto operator=(const vector_t& other) -> vector_t&;
-    ~vector_t() noexcept;
-    auto size() const noexcept -> unsigned int;
-    auto capacity() const noexcept -> unsigned int;
-    auto push_back(int value) -> void;
-    auto operator[](unsigned int index) const noexcept -> int;
+	vector_t() noexcept;
+	vector_t(unsigned int size);
+	vector_t(const vector_t& other);
+	auto operator=(const vector_t& other)->vector_t&;
+	~vector_t() noexcept;
+	auto size() const noexcept -> unsigned int;
+	auto capacity() const noexcept -> unsigned int;
+	auto push_back(int value) -> void;
+	auto pop_back() -> void;
+	vector_t(std::initializer_list<int> list);
+	auto operator[](unsigned int index) const noexcept -> int;
 };
