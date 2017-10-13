012ded3ff3a17b3b1e6d675d72741cc05baba0e4
NeverMore27 <uragan.uragan.59@mail.ru>
2017-10-13 21:57:04 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,70 +1,44 @@
 
 #include <vector.hpp>
 #include <catch.hpp>
-SCENARIO("NULL") 
+SCENARIO("null")
 {
-	vector_t vector;
-	REQUIRE(vector.size() == 0);
-	REQUIRE(vector.capacity() == 0);
-}
-SCENARIO("size")
-{
-	vector_t vector;
-	REQUIRE(vector.size() == 0);
-	vector.push_back(2) ;
-	REQUIRE(vector.size() == 1);
-
-}
-SCENARIO("capacity")
-{
-	vector_t vector;
-	REQUIRE(vector.capacity() == 0);
-	vector.push_back(2) ;
-	REQUIRE(vector.capacity() == 2);
-
-}
-SCENARIO("param") 
-{
-	vector_t vector(3);
-	REQUIRE(vector.size() == 3);
-	REQUIRE(vector.capacity() == 3);
+	stack<int> Stack;
+	Stack.push(5);
+	Stack.push(7);
+	int val = Stack.pop();
+		REQUIRE(Stack.count() == 2);
+	REQUIRE(val == 7);
 }
 
-SCENARIO("push") 
+SCENARIO("null")
 {
-	vector_t vector(3);
-	vector.push_back(2) ;
-	REQUIRE(vector[3]==2);
-	REQUIRE(vector.size() == 4);
-	REQUIRE(vector.capacity() == 5);
+	stack<int> Stack;
+	Stack.push(5);
+	Stack.push(7);
+	Stack.push(9);
+	int val = Stack.pop();
+	REQUIRE(Stack.count() == 2);
+	REQUIRE(val == 9);
 }
-SCENARIO("element") 
+
+SCENARIO("null")
 {
-	vector_t vector(3);
-	vector.push_back(2) ;
-	REQUIRE(vector[3]==2);
+	stack<int> Stack;
+	REQUIRE(Stack.count() == 0);
 }
-SCENARIO("=") 
+SCENARIO("null")
 {
-	vector_t vector1(3), vector;
-	vector1.push_back(2) ;
-	vector=vector1;
-	REQUIRE(vector[3]==2);
-	REQUIRE(vector.size()==4);
-	REQUIRE(vector.capacity()==5);
-}
-SCENARIO("Copy") 
-{
-	vector_t v (3);
-	vector_t vector(v);
-	REQUIRE(vector.size() == v.size());
-	REQUIRE(vector.capacity() == v.capacity());
-}
-SCENARIO("del") 
-{
-	vector_t v={1,2,3,5,7,8,1,2,4};
-	vector_t vector={1,2,3,5,7,8,1};
-	v.pop_back();
-	v.pop_back();
-	REQUIRE(v==vector);
+	stack<int> Stack;
+	Stack.push(5);
+	Stack.push(7);
+	Stack.push(9);
+	stack<int> s(Stack);
+	REQUIRE(s.count() == 3);
+	int val = s.pop();
+	REQUIRE(val == 9);
+	val = s.pop();
+	REQUIRE(val == 7);
+	val = s.pop();
+	REQUIRE(val == 5);
 }
