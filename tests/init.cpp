bfe497f71effb5da9eb03b8071540938b3e177b1
NeverMore27 <uragan.uragan.59@mail.ru>
2017-10-13 23:35:03 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -28,18 +28,3 @@ SCENARIO("null2")
 	stack<int> Stack;
 	REQUIRE(Stack.count() == 0);
 }
-SCENARIO("null3")
-{
-	stack<int> Stack;
-	Stack.push(5);
-	Stack.push(7);
-	Stack.push(9);
-	stack<int> s(Stack);
-	REQUIRE(s.size() == 3);
-	int val = s.pop();
-	REQUIRE(val == 9);
-	val = s.pop();
-	REQUIRE(val == 7);
-	val = s.pop();
-	REQUIRE(val == 5);
-}
