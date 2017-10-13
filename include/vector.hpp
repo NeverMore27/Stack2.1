c8388297eae811f33a5941e727837a70d6e9c3f0
NeverMore27 <uragan.uragan.59@mail.ru>
2017-10-13 22:51:50 +0300

    Update vector.hpp

--- include/vector.hpp
+++ include/vector.hpp
@@ -16,12 +16,12 @@ private:
 	T * array_;
 	size_t array_size_;
 	size_t count_;
-	void swap(size_t, size_t, const T*, T*&);
+	void swap(size_t, const T*, T*&);
 };
 template <typename T>
-void stack<T>::swap(size_t count, size_t size, const T* _ar, T*& ar)
+void stack<T>::swap(size_t count, const T* _ar, T*& ar)
 {
-	ar = new T[size];
+	ar = new T[count];
 	std::copy(_ar, _ar + count, ar);
 }
 template <typename T>
@@ -43,7 +43,7 @@ stack<T>::stack(const stack& object)
 {
 	array_size_ = object.array_size_;
 	count_ = object.count_;
-	swap(count_, array_size_, object.array_, array_);
+	swap(count_, object.array_, array_, );
 }
 template <typename T>
 stack<T>& stack<T>:: operator =(const stack<T>&object)
@@ -53,36 +53,58 @@ stack<T>& stack<T>:: operator =(const stack<T>&object)
 		array_size_ = object.array_size_;
 		count_ = object.count_;
 		delete[] array_;
-		swap(count_, array_size_, object.array_, array_);
+		swap(count_, object.array_, array_, );
 	}
 	return *this;
 }
 template <typename T>
 void stack<T>::push(T const &value)
 {
-	if (array_size_ == 0)
+	if (count_ == 0)
 	{
-		count_=2; 
-		array_size_++;
-		array_ = new T[array_size_];
-		array_[0] = value;
+		
+			count_ = 1;
+			array_size_ = 1;
+			array_ = new T[count_];
+			array_[0] = value;
 	}
-	if (array_size_ == count_)
+	else 
 	{
-		T *ptr = array_;
-		int capacity = count_ * 2;
-		swap(count_, array_size_, ptr, array_);
-		count_ = capacity;
-		delete[] ptr;
+		if (array_size_ == count_)
+		{
+			T *ptr = array_;
+			count_ = count_ * 2;
+			swap(count_, ptr, array_);
+			delete[] ptr;
+		}
+		array_[array_size_++] = value;
 	}
-	array_[count_++] = value;
 }
 template <typename T>
 T stack<T>::pop()
 {
-	if (count_ == 0) throw "Stack is empty";
-	--count_;
-	return array_[count_];
+	if (count_ > 0) 
+	{
+		T value = array_[--array_size_];
+		if (array_size_ == 0)
+		{
+			delete[] array_;
+			count_ = 0;
+		}
+		else {
+			if (array_size_ * 2 == count_) {
+				T *ptr = array_;
+				array_size_ /= 2;
+				swap(count_, ptr, array_);
+				delete[] ptr;
+			}
+		}
+		return value;
+	}
+	else 
+	{
+		throw "Stack is empty";
+	}
 }
 template <typename T>
 size_t stack<T>::count() const
