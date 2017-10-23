32835312d1f6016e011559ab6d6c73408073a4ca
NeverMore27 <uragan.uragan.59@mail.ru>
2017-10-23 12:51:05 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -5,7 +5,7 @@ template <typename T>
 class stack
 {
 public:
-	stack() /*noexcept*/; 
+	stack() /*noexcept*/ noexcept; 
 	~stack()  /*noexcept*/ noexcept;
 	stack(const stack<T>&) /*no safety*/;
 	stack<T>& operator =(const stack<T>&) /*no safety*/;
