d92351ea4026cbb7d9d43b40e7edce2146b51889
NeverMore27 <uragan.uragan.59@mail.ru>
2017-10-30 11:23:14 +0300

    Update stack.cpp

--- sources/stack.cpp
+++ sources/stack.cpp
@@ -1,1 +1,36 @@
 #include "stack.hpp"
+#include <thred>
+#include <string>
+
+
+template <typename T>
+void produser (stck<T> &st)
+{
+   for (;;)
+    {
+      st.push(rand()%10);
+      std::this_thread::sleep_for(std::chrono::seconds(std::rand() % (3) + 1));
+    }
+}
+
+template <typename T>
+void consumer (stck<T> &st)
+{
+   for (;;)
+    {
+       st.pop();
+       std::this_thread::sleep_for(std::chrono::seconds(std::rand() % (3) +2 ));
+    }
+}
+
+int main()
+{
+	stack<int> Stack;
+	
+	std::thread p(producer, std::ref(Stack));
+	std::thread c(consumer, std::ref(Stack));
+	
+	p.join();
+	c.join();
+  return 0;
+}
