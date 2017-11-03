84f16b7abac399de05ef06c50abe79c1fdf60649
NeverMore27 <uragan.uragan.59@mail.ru>
2017-11-04 00:13:46 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -105,7 +105,7 @@ auto stack<T>::pop() -> std::shared_ptr<T>
 {
 	std::lock_guard<std::mutex> lock(mutex_);
 	if (count_==0) throw ("Stack is empty");
-	auto ar = std::make_shared<int>(array_[count-1]);
+	auto ar = std::make_shared<int>(array_[count_-1]);
 	--count_;
 	return ar;
 }
