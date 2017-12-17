91ce5fcfa7b53c07109507533c34a94021895a6d
NeverMore27 <uragan.uragan.59@mail.ru>
2017-12-17 17:03:01 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -14,8 +14,9 @@ SCENARIO("push")
 	Stack.push(5);
 	Stack.push(7);
 	Stack.push(12);
-	int sp =*Stack.pop();
-	bool d=(*sp==12);
+	int sp =Stack.top();
+	Stack.pop()
+	bool d=(sp==12);
 	REQUIRE(Stack.size() == 2);
 	REQUIRE(d);
 }
